e22f19a78cb6588a0b8aaad6f1d53b5f6093a42e
doom <phramov945@gmail.com>
2022-12-20 16:04:48 +0300

    admin panel customization v 1.0

---
+++ core/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from core.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", )
+    search_fields = ("email", "first_name", "last_name", "username", )
+    list_filter = ("is_staff", "is_active", "is_superuser", )
+    exclude = ("password", "user_permissions", "groups")
+    readonly_fields = ("last_login", "date_joined", )
