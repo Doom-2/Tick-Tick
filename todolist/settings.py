703929ebbed0711d48010e9086da6e94c1a56d39
doom <phramov945@gmail.com>
2022-12-20 15:48:23 +0300

    DB engine was changed to Postgres

--- todolist/settings.py
+++ todolist/settings.py
@@ -83,10 +83,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL'),
+
+    'extra': env.db_url(
+        'SQLITE_URL',
+        default='sqlite:////tmp/my-tmp-sqlite.db'
+    )
 }
 
 
