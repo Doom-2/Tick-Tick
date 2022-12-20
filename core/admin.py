9f0d9618bb4b5528b3a2226694c40f7a4703ae26
doom <phramov945@gmail.com>
2022-12-20 16:35:42 +0300

    admin panel customization v 1.1

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from core.models import User
 
@@ -10,3 +11,6 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ("is_staff", "is_active", "is_superuser", )
     exclude = ("password", "user_permissions", "groups")
     readonly_fields = ("last_login", "date_joined", )
+
+
+admin.site.unregister(Group)
