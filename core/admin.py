a245565e84844edc469ed1314199a464383d351c
doom <phramov945@gmail.com>
2023-01-05 02:32:42 +0300

    Use pre-commit

--- core/admin.py
+++ core/admin.py
@@ -7,11 +7,11 @@ from core.models import User
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    list_display = ("username", "email", "first_name", "last_name", )
-    search_fields = ("email", "first_name", "last_name", "username", )
-    list_filter = ("is_staff", "is_active", "is_superuser", )
-    exclude = ("password", )
-    readonly_fields = ("last_login", "date_joined", )
+    list_display = ('username', 'email', 'first_name', 'last_name', )
+    search_fields = ('email', 'first_name', 'last_name', 'username', )
+    list_filter = ('is_staff', 'is_active', 'is_superuser', )
+    exclude = ('password', )
+    readonly_fields = ('last_login', 'date_joined', )
 
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
