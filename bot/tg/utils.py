65b970731660b0c574aecc738b5e20f476d56160
doom <phramov945@gmail.com>
2023-02-03 16:45:09 +0300

    Improve bot interaction logic

---
+++ bot/tg/utils.py
@@ -0,0 +1,18 @@
+from typing import List, Tuple
+
+
+def numerate_cats_tuple(cat_set: List[Tuple]) -> str:
+    """
+    Convers list of tuple to string with line- by-line output
+    [('1', 'var1'), ('23', 'var2'), ('34', 'var3')]
+    ________
+    1  var1
+    23 var2
+    34 var 3
+    """
+    return '\n'.join(map(lambda x: '/' + str(x[0]) + ' ' + str(x[1].replace('#', '%23')), cat_set))
+
+
+def fix_hash_char(lst: list):
+    """ Replaces the # (hash) character with %23 for pretty output in messenger Telegram"""
+    return [sub.replace('#', '%23') for sub in lst]
