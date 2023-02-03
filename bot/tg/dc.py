80093fbd1909400579bf7f2750049d50cb48a97f
doom <phramov945@gmail.com>
2023-02-03 07:53:28 +0300

    Implement long polling for bot

---
+++ bot/tg/dc.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+from marshmallow import EXCLUDE
+
+
+@dataclass
+class MessageFrom:
+    id: int
+    is_bot: bool
+    first_name: str
+    username: str
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class Chat:
+    id: int
+    first_name: str
+    username: str
+    type: str
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class Message:
+    message_id: int
+    from_: MessageFrom = field(metadata=dict(data_key='from', attribute='from_'))
+    chat: Chat
+    date: int
+    text: str
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class UpdateObj:
+    update_id: int
+    message: Optional[Message]
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class GetUpdatesResponse:
+    ok: bool
+    result: List[UpdateObj]
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class SendMessageResponse:
+    ok: bool
+    result: Message
+
+    class Meta:
+        unknown = EXCLUDE
