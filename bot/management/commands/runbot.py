f9e870014dca5e16fe73a51812fe8fce7dd73b00
doom <phramov945@gmail.com>
2023-02-03 16:45:09 +0300

    Improve bot interaction logic

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -1,6 +1,7 @@
 from django.core.management import BaseCommand
 from bot.models import TgUser
 from bot.tg.client import TgClient
+from bot.tg.utils import numerate_cats_tuple, fix_hash_char
 from bot.password_generator import pwd_generator
 from goals.models import GoalCategory, Goal
 import environ
@@ -16,8 +17,10 @@ class Command(BaseCommand):
         super().__init__()
         self.tg_client = TgClient(env('TG_CLIENT_TOKEN'))
         self.category_set: list = []
+        self.category_ids: list = []
+        self.goal_set: list = []
         self.category_selection_state: bool = False
-        self.selected_category: str = ''
+        self.selected_category: GoalCategory | None = None
         self.goal_title_state: bool = False
         self.logger = logging.getLogger(__name__)
         self.logger.info('Bot start polling')
@@ -39,6 +42,8 @@ class Command(BaseCommand):
                             tg_chat_id=item.message.chat.id,
                             defaults={'tg_user_name': item.message.from_.username}
                         )
+
+                        # tg_user writes to bot for the first time
                         if created:
                             self.tg_client.send_message(item.message.chat.id,
                                                         'Nice to meet you!.'
@@ -48,7 +53,8 @@ class Command(BaseCommand):
                             tg_user.verification_code = pwd
                             tg_user.save()
 
-                        elif not created and not tg_user.user_id:
+                        # tg_user exists, but does not verify the code
+                        elif not tg_user.user_id:
                             self.tg_client.send_message(item.message.chat.id,
                                                         'Amigo...😁\nTo continue using the service, '
                                                         'please confirm the verification code on '
@@ -58,78 +64,105 @@ class Command(BaseCommand):
                             tg_user.verification_code = pwd
                             tg_user.save()
 
-                        elif not created and tg_user.user_id and item.message.text == '/cancel':
+                        # /cancel entered
+                        elif tg_user.user_id and item.message.text == '/cancel':
                             self.tg_client.send_message(item.message.chat.id,
-                                                        'Operation cancelled.\n'
-                                                        'Enter /goals to view all your goals.\n'
+                                                        'Operation cancelled.\n\n'
+                                                        'Enter /goals to view all your goals.\n\n'
                                                         'Enter /create to create a new goal')
                             self.category_selection_state = False
                             self.goal_title_state = False
 
-                        elif not created and tg_user.user_id and self.category_selection_state \
-                                and item.message.text not in self.category_set and not self.goal_title_state:
-                            self.tg_client.send_message(item.message.chat.id,
-                                                        'Category doesn\'t exist\n'
-                                                        'Enter an existing one or /cancel to cancel the operation')
-
-                        elif not created and tg_user.user_id and item.message.text not in ['/goals', '/create'] \
-                                and not self.category_selection_state and not self.goal_title_state:
-                            self.tg_client.send_message(item.message.chat.id, 'I don\'t understand you ¯\\_(ツ)_/¯\n\n'
-                                                                              'Enter /goals to view all your goals.\n\n'
-                                                                              'Enter /create to create a new goal.')
-
-                        elif not created and tg_user.user_id and item.message.text == '/goals':
+                        # '/goal' entered, displays list of user goals on boards, where he is owner or writer
+                        elif tg_user.user_id and item.message.text == '/goals':
                             category_set = GoalCategory.objects.all().filter(
                                 board__participants__user_id=tg_user.user_id,
-                                is_deleted=False
+                                is_deleted=False,
+                                board__participants__role__in=[1, 2]
                             )
-                            goals_set = list(Goal.objects.filter(
+                            self.goal_set = list(Goal.objects.filter(
                                 status__in=[1, 2, 3],
                                 category__in=category_set
                             ).values_list('title', flat=True))
 
-                            self.tg_client.send_message(item.message.chat.id, '\n'.join(goals_set))
+                            self.goal_set = fix_hash_char(self.goal_set)
 
-                        elif not created and tg_user.user_id and item.message.text == '/create' \
+                            self.tg_client.send_message(item.message.chat.id, 'List of your goals:\n\n' +
+                                                        '\n'.join(self.goal_set))
+                            self.logger.info('\n'.join(self.goal_set))
+
+                        # '/create' entered, displays list of user categories on boards, where he is owner or writer
+                        elif tg_user.user_id and item.message.text == '/create' \
                                 and not self.category_selection_state and not self.goal_title_state:
                             self.category_set = list(
                                 GoalCategory.objects.filter(board__participants__user_id=tg_user.user_id,
-                                                            is_deleted=False).values_list('title',
-                                                                                          flat=True))
+                                                            is_deleted=False,
+                                                            board__participants__role__in=[1, 2]
+                                                            ).values_list('id', 'title', ))
+
+                            self.category_ids = [x[0] for x in self.category_set]
                             self.tg_client.send_message(item.message.chat.id,
-                                                        '\n'.join(self.category_set))
-                            self.logger.info('\n'.join(self.category_set))
+                                                        'Click on the number to select a category: \n\n'
+                                                        + numerate_cats_tuple(self.category_set))
+                            self.logger.info(numerate_cats_tuple(self.category_set))
                             self.category_selection_state = True
 
-                        elif not created and tg_user.user_id and item.message.text in self.category_set \
-                                and self.category_selection_state:
-                            self.selected_category = GoalCategory.objects.get(title=item.message.text)
+                        # non-existing category entered
+                        elif tg_user.user_id and self.category_selection_state and len(item.message.text) >= 2 \
+                                and item.message.text[1:].isdigit() \
+                                and int(item.message.text[1:]) not in self.category_ids:
+                            self.tg_client.send_message(item.message.chat.id,
+                                                        'Category doesn\'t exist\n'
+                                                        'Enter an existing one or /cancel to cancel the operation')
+
+                        # existing category entered
+                        elif tg_user.user_id and self.category_selection_state and len(item.message.text) >= 2 \
+                                and item.message.text[1:].isdigit() \
+                                and int(item.message.text[1:]) in self.category_ids:
+                            self.selected_category = GoalCategory.objects.get(id=int(item.message.text[1:]))
                             self.tg_client.send_message(item.message.chat.id,
                                                         'Great! Now enter goal title\n'
                                                         'or /cancel to cancel the operation')
                             self.goal_title_state = True
 
-                        elif not created and tg_user.user_id and self.category_selection_state \
+                        # category created
+                        elif tg_user.user_id and self.category_selection_state \
                                 and self.goal_title_state:
                             new_goal = Goal.objects.create(title=item.message.text,
                                                            user_id=tg_user.user_id,
                                                            category=self.selected_category)
                             category_set = GoalCategory.objects.filter(
                                 board__participants__user_id=tg_user.user_id,
-                                is_deleted=False
+                                is_deleted=False,
+                                board__participants__role__in=[1, 2]
                             )
-                            goals_set = list(Goal.objects.filter(
+                            self.goal_set = list(Goal.objects.filter(
                                 status__in=[1, 2, 3],
                                 category__in=category_set
                             ).values_list('title', flat=True))
 
-                            goals_set_by_line = '\n'.join(goals_set)
+                            new_goal.title = new_goal.title.replace('#', '%23')
+                            self.goal_set = fix_hash_char(self.goal_set)
                             self.tg_client.send_message(item.message.chat.id,
-                                                        f'Goal \'{new_goal.title}\' has been successfully created.\n'
-                                                        'List of your goals:\n\n'
-                                                        f'{goals_set_by_line}')
+                                                        f'Goal \'{new_goal.title}\' has been successfully created.\n\n'
+                                                        'List of your goals:\n' +
+                                                        '\n'.join(self.goal_set))
+                            self.logger.info('\n'.join(self.goal_set))
                             self.category_selection_state = False
                             self.goal_title_state = False
+
+                        # all other cases
+                        else:
+                            self.tg_client.send_message(item.message.chat.id,
+                                                        'I don\'t understand you ¯\\_(ツ)_/¯\n\n'
+                                                        'Enter /goals to view all your goals.\n\n'
+                                                        'Enter /create to create a new goal.')
+                            self.category_selection_state = False
+                            self.goal_title_state = False
+                            self.selected_category = None
+                            self.category_set = []
+                            self.category_ids = []
+                            self.goal_set = []
 
         except KeyboardInterrupt:
             self.logger.info('\nPolling has finished')
